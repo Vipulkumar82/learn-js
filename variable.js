c1c58e6b556088340b28c2eef88bf403647e52a1
vipulkumar82 <vipulkumar212003@gmail.com>
2024-07-29 13:59:48 +0530

    var information

---
+++ variable.js
@@ -0,0 +1,15 @@
+// type of variables
+// let , const , var
+let var1 = 'vipul';
+let var2 = 32;
+let var3 = ['vipul','vicky','lala'];
+var1 = 'vicky';
+
+
+const name1 = 'lala'
+
+
+console.log(name1)
+console.log(var1)
+console.log(var2)
+console.log(var3)
