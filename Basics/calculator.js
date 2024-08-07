28906c354b92b082d323e6dc74b191f1e4b22888
vipulkumar82 <vipulkumar212003@gmail.com>
2024-08-07 19:54:12 +0530

    Arrow Function

--- Basics/calculator.js
+++ Basics/calculator.js
@@ -1,26 +1,96 @@
-// let a=10;
-// let b=40;
-// let operator = '*';
-// let result ;
+let a=10;
+let b=40;
+let operator = '*';
+let result ;
 
-// switch(operator){
-//     case '+':
-//         console.log(`addition = ${a+b}`);
-//         break;
-//     case '-':
-//         console.log(`substraction = ${a-b}`);
-//         break;
-//     case '*':
-//         console.log(`multiplication = ${a*b}`);
-//         break;
-//     case '/':
-//         console.log(`division = ${a/b}`);
-//         break;
-//     default:
-//         console.log("invalid operation please enter + or - or * or /");
-//         break;
+switch(operator){
+    case '+':
+        console.log(`addition = ${a+b}`);
+        break;
+    case '-':
+        console.log(`substraction = ${a-b}`);
+        break;
+    case '*':
+        console.log(`multiplication = ${a*b}`);
+        break;
+    case '/':
+        console.log(`division = ${a/b}`);
+        break;
+    default:
+        console.log("invalid operation please enter + or - or * or /");
+        break;
+}
+
+//                    CALCULATOR USING   " ARROW FUNCTION "
+
+// normal function
+// function addition (a,b) {
+//     return a+b;
 // }
 
+// // arrow function
+// // 1
+// let substraction = (a,b) => {
+//     return a-b;
+// }
+
+// // 2
+// let multiplication = (a,b) => a*b;
+
+// // 3
+// let print = a => console.log(a)
+
+
+
+
+//make a code for applying +,-,*,/ using array function with if else statement 
+
+// let a=50;
+// let b=2;
+// let operator="*";
+// let result;
+
+// // addition function defination using arrow function
+// let addition=(a,b)=>a+b;
+
+// // substraction function defination using arrow function
+// let substraction=(a,b) => a-b;
+
+// // multiplication function defination using arrow function
+// let multiplication=(a,b)=>a*b;
+
+// // division function defination using arrow function
+// let division=(a,b)=>a/b;
+
+// // modular division function defination using arrow function
+// let modular=(a,b)=>a%b;
+
+// // for printing result funtion is:-
+// let print=result=>console.log(result);
+
+// // make a calculator using if else or if else if else 
+// if(operator=="+"){
+//     result=addition(a,b);
+//     print(result);
+// }
+// if(operator=="-"){
+//     result=substraction(a,b);
+//     print(result);
+// }
+// if(operator=="*"){
+//    result=multiplication(a,b);
+//    print(result);
+// }
+// if(operator=="/"){
+//     result=division(a,b);
+//     print(result);
+// }
+// if(operator=="%"){
+//     result=modular(a,b);
+//     print(result);
+// }
+
+//General Informations
 
 // while
 // do while
@@ -78,77 +148,6 @@
 
 // const c = names.map(value=>value*3)
 // console.log(c);
-
-
-
-// normal function
-// function addition (a,b) {
-//     return a+b;
-// }
-
-// // arrow function
-// // 1
-// let substraction = (a,b) => {
-//     return a-b;
-// }
-
-// // 2
-// let multiplication = (a,b) => a*b;
-
-// // 3
-// let print = a => console.log(a)
-
-
-
-
-//make a code for applying +,-,*,/ using array function with if else statement 
-
-let a=50;
-let b=2;
-let operator="*";
-let result;
-
-// addition function defination using arrow function
-let addition=(a,b)=>a+b;
-
-// substraction function defination using arrow function
-let substraction=(a,b) => a-b;
-
-// multiplication function defination using arrow function
-let multiplication=(a,b)=>a*b;
-
-// division function defination using arrow function
-let division=(a,b)=>a/b;
-
-// modular division function defination using arrow function
-let modular=(a,b)=>a%b;
-
-// for printing result funtion is:-
-let print=result=>console.log(result);
-
-// make a calculator using if else or if else if else 
-if(operator=="+"){
-    result=addition(a,b);
-    print(result);
-}
-if(operator=="-"){
-    result=substraction(a,b);
-    print(result);
-}
-if(operator=="*"){
-   result=multiplication(a,b);
-   print(result);
-}
-if(operator=="/"){
-    result=division(a,b);
-    print(result);
-}
-if(operator=="%"){
-    result=modular(a,b);
-    print(result);
-}
-
-
 
 
 
