5119b02fff3128b236329b9bca5a2fff9ec1cc4e
vipulkumar82 <vipulkumar212003@gmail.com>
2024-08-20 15:16:09 +0530

    stack and Heap

--- Basics/datatype_summary.js
+++ Basics/datatype_summary.js
@@ -12,7 +12,7 @@ let userEmail;
 const id = Symbol('123')
 const anotherId = Symbol('123')
 
-console.log(id === anotherId);
+// console.log(id === anotherId);
 
 // const bigNumber = 3456543576654356754n
 
@@ -29,9 +29,37 @@ let myObj = {
 }
 
 const myFunction = function(){
-    console.log("Hello world");
+    // console.log("Hello world");
 }
 
-console.log(typeof anotherId);
+// console.log(typeof anotherId);
 
-// https://262.ecma-international.org/5.1/#sec-11.4.3
+// https://262.ecma-international.org/5.1/#sec-11.4.3    read for better understanding 
+
+//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+// Stack(Primitive type(isme copy milta hai))  and
+// Heap (Non primitive (isme reference milta hai ))
+
+let myYTname="vipul8298";
+
+let otherYTname=myYTname
+otherYTname="thakur8298"
+console.log(otherYTname); //thakur8298
+console.log(myYTname);   //vipul8298
+
+let userOne={
+    email: "user@gmail.com",
+    upi: "user@sbi"
+}
+
+let userTwo=userOne
+
+userTwo.email="user2@gmail.com"
+
+console.log(userOne.email)
+console.log(userTwo.email)  
+// both will be user2 emial
+// because in Heap the reference will come 
+// if we change in one then second will also change
+
