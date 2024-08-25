c8c60f239065f304160d62b810d4c6c16e41e6b8
vipulkumar82 <vipulkumar212003@gmail.com>
2024-08-26 03:46:17 +0530

    modified

--- iteration/for_loop.js
+++ iteration/for_loop.js
@@ -19,7 +19,7 @@ for (let i = 1; i <= 10; i++) {
    }
     
 }
-let myArray = ["flash", "batman", "superman"]
+//let myArray = ["flash", "batman", "superman"]
 //console.log(myArray.length);
 for (let index = 0; index < myArray.length; index++) {
     const element = myArray[index];
@@ -39,11 +39,34 @@ for (let index = 0; index < myArray.length; index++) {
     
 // }
 
-for (let index = 1; index <= 20; index++) {
-    if (index == 5) {
-        console.log(`Detected 5`);
-        continue
-    }
-   console.log(`Value of i is ${index}`);
+// for (let index = 1; index <= 20; index++) {
+//     if (index == 5) {
+//         console.log(`Detected 5`);
+//         continue
+//     }
+//    console.log(`Value of i is ${index}`);
     
+// }
+
+
+
+let index = 0
+// while (index <= 10) {
+//     console.log(`Value of index is ${index}`);
+//     index = index + 2
+// }
+
+let myArray = ['flash', "batman", "superman"]
+
+let arr = 0
+while (arr < myArray.length) {
+    //console.log(`Value is ${myArray[arr]}`);
+    arr = arr + 1
 }
+
+let score = 11
+
+do {
+    console.log(`Score is ${score}`);
+    score++
+} while (score <= 10);
