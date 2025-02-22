2e5096a226f5c8395de79ac410b0e5d9272b1708
vipulkumar82 <vipulkumar212003@gmail.com>
2025-02-22 18:23:42 +0530

    project Completed

--- projects/script.js
+++ projects/script.js
@@ -1,22 +1,24 @@
 const body=document.querySelector("body");
-const buttons = document.querySelectorAll(".buttons");
+const buttons=document.querySelectorAll(".buttons");
 
-buttons.forEach(function(button){
-    button.addEventListener('click',function(e){
-        // console.log(e);
-        // console.log(e.target);
 
-        if(e.target.id==="green"){
-            body.style.backgroundColor="green";
-        }
-        if(e.target.id==="red"){
+
+buttons.forEach(function (button){
+    button.addEventListener("click",function(e){
+        // console.log(e);  //events
+        // console.log(e.target);   //forgetting Target event
+
+        if(e.target.id=="blue"){
             body.style.backgroundColor=e.target.id;
         }
-        if(e.target.id==="purple"){
+        if(e.target.id=="red"){
+            body.style.backgroundColor="red";
+        }
+        if(e.target.id=="purple"){
             body.style.backgroundColor=e.target.id;
         }
-        if(e.target.id==="blue"){
+        if(e.target.id=="green"){
             body.style.backgroundColor=e.target.id;
         }
-    })
-})
+    });
+});
