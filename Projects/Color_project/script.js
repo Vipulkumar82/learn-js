f2e5c2a0e0976d6f72af0a0e262558f602429b2f
vipulkumar82 <vipulkumar212003@gmail.com>
2025-02-28 00:01:51 +0530

    events Basics

--- Projects/Color_project/script.js
+++ Projects/Color_project/script.js
@@ -1,32 +1,43 @@
 const body=document.querySelector("body");
-const buttons=document.querySelectorAll(".container")[0];
+const buttons=document.querySelectorAll(".buttons");
 
-buttons.addEventListener("click",function(e){
-        e.stopPropagation();
-        
-        // console.log(e);  //events
-        // console.log(e.target);   //forgetting Target event
-        
-        if(e.target.id=="blue"){
-            body.style.backgroundColor=e.target.id;
-            console.log("blue");
-        }
-        if(e.target.id=="red"){
-            body.style.backgroundColor="red";
-            console.log("red");
-        }
-        if(e.target.id=="purple"){
-            body.style.backgroundColor=e.target.id;
-            console.log("purple");
-        }
-        if(e.target.id=="green"){
-            body.style.backgroundColor=e.target.id;
-            console.log("Green");
-        }
-        console.log("hi");
-},true);
+buttons.forEach(function(button){     //Using ForEach loop
+button.addEventListener("click",function(e){
+    // console.log(e);  //events
+    // console.log(e.target);   //forgetting Target event
     
-body.addEventListener("click",function(e){
-    console.log("body clicked");
-    body.style.backgroundColor="white";
+    if(e.target.id=="blue"){
+        body.style.backgroundColor=e.target.id;
+    }
+    if(e.target.id=="red"){
+        body.style.backgroundColor="red";
+    }
+    if(e.target.id=="purple"){
+        body.style.backgroundColor=e.target.id;
+    }
+    if(e.target.id=="green"){
+        body.style.backgroundColor=e.target.id;
+    }
 });
+});
+
+
+// const body=document.querySelector('body');
+// const buttons=document.querySelectorAll('.container')[0];  
+// // instead of using ForEach loop we used eventDeligation
+// // we take main parent div and put eventListener so any chiled is clicked then ..
+
+// buttons.addEventListener('click',function(e){
+//     if(e.target.id=='green'){
+//         body.style.backgroundColor=e.target.id;
+//     }
+//     if(e.target.id=='red'){
+//         body.style.backgroundColor=e.target.id;
+//     }
+//     if(e.target.id=='purple'){
+//         body.style.backgroundColor=e.target.id;
+//     }
+//     if(e.target.id=='blue'){
+//         body.style.backgroundColor=e.target.id;
+//     }
+// })
