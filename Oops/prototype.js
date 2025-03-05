1d594e2c6fc3bdefdac9d5d4cd71351966f71484
vipulkumar82 <vipulkumar212003@gmail.com>
2025-03-05 16:58:37 +0530

    prototype

---
+++ Oops/prototype.js
@@ -0,0 +1,23 @@
+const naam="vipul";
+console.log(naam.length); //5 
+
+const naaam="vipul     ";
+console.log(naaam.length); // 10 
+console.log(naaam.trim().length) //5
+/*
+and we want to remove the spaces count 
+without using naam.trim().length 
+..Make a .trueLength named method for this work 
+*/
+
+String.prototype.trueLength = function(){
+    console.log(this);
+    console.log(`True length is ${this.trim().length}`)
+};
+
+naaam.trueLength()
+"hitesh Sir  ".trueLength();
+
+//this is how we create our method using prototype 
+// and if we make the method direct to Object.prototype 
+// Then it will working in all like string array object function ..
